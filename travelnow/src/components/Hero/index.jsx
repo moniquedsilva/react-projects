94f8da8f0795af2f25aa87b0dc2a1260ea1d061a
Monique Silva <moniquesntsilva@gmail.com>
2021-11-03 23:20:05 -0300

    feat: mobile hero section

---
+++ travelnow/src/components/Hero/index.jsx
@@ -0,0 +1,28 @@
+import './hero.css';
+
+export default function Hero() {
+  return (
+    <section className='hero-section'>
+      <div className='hero-content'>
+        <h1>Find your next place to travel</h1>
+        <p>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type.
+        </p>
+        <div className='hero-search'>
+          <label htmlFor='search'>Search</label>
+          <input
+            type='text'
+            id='search'
+            name='search'
+            placeholder='Burj khalifa, Dub'
+          />
+          <button>Search</button>
+        </div>
+      </div>
+      <picture className='hero-picture'>
+        <img src='./assets/figures/car.svg' alt='Car' className='hero-img' />
+      </picture>
+    </section>
+  );
+}
--- travelnow/src/pages/Home.jsx
+++ travelnow/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import '../styles/index.css';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 export default function Home() {
   return (
     <div className='main-container'>
       <Header />
+      <Hero />
     </div>
   );
 }
