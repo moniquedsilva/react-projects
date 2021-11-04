da6dc8a297ea3840169bdf9ef06e9da98b6c5e9f
Monique Silva <moniquesntsilva@gmail.com>
2021-11-03 22:07:58 -0300

    feat: add main container

--- travelnow/src/pages/Home.jsx
+++ travelnow/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <>
+    <div className='main-container'>
       <Header />
-    </>
+    </div>
   );
 }
