88f772bed9b8b620fd594bb0a468c767ba5476cc
Monique Silva <moniquesntsilva@gmail.com>
2021-11-05 15:30:05 -0300

    feat: include missing part

---
+++ travelnow/src/components/Destination/data.jsx
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 0,
+    title: 'Years Experience',
+    value: 20,
+  },
+  {
+    id: 1,
+    title: 'Destination Collaboration',
+    value: 55,
+  },
+  {
+    id: 2,
+    title: 'Tourist Destination',
+    value: '600+',
+  },
+];
--- travelnow/src/components/Destination/index.jsx
+++ travelnow/src/components/Destination/index.jsx
@@ -1,4 +1,5 @@
 import './destination.css';
+import { data } from './data';
 
 export default function Destination() {
   return (
@@ -8,10 +9,20 @@ export default function Destination() {
       </picture>
       <div className='destination-content'>
         <h2>Travel to any corner of the World</h2>
-        <p>
+        <p className='destination-paragraph'>
           Lorem Ipsum has been the industry's standard dummy text ever since the
           1500s, when an unknown printer took a galley of type.
         </p>
+        <div className='destination-info'>
+          {data.map((item) => {
+            return (
+              <div key={item.id} className='info-cards'>
+                <span className='info-span'>{item.value}</span>
+                <p className='info-paragraph'>{item.title}</p>
+              </div>
+            );
+          })}
+        </div>
         <button className='destination-btn'>Explore Destination</button>
       </div>
     </section>
