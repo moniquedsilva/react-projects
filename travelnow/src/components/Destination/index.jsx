5db04d6d53be84d82858a26ee3c6d88444f1fade
Monique Silva <moniquesntsilva@gmail.com>
2021-11-05 14:39:47 -0300

    feat: add mobile version

--- travelnow/src/components/Destination/index.jsx
+++ travelnow/src/components/Destination/index.jsx
@@ -2,17 +2,17 @@ import './destination.css';
 
 export default function Destination() {
   return (
-    <section>
-      <picture>
+    <section className='destination-section'>
+      <picture className='destination-img'>
         <img src='./assets/figures/travel.svg' alt='Travel couple' />
       </picture>
-      <div>
+      <div className='destination-content'>
         <h2>Travel to any corner of the World</h2>
         <p>
           Lorem Ipsum has been the industry's standard dummy text ever since the
           1500s, when an unknown printer took a galley of type.
         </p>
-        <button>Explore Destination</button>
+        <button className='destination-btn'>Explore Destination</button>
       </div>
     </section>
   );
