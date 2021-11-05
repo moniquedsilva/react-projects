6b0f01b672bcd2239caf591375195baff75772c6
Monique Silva <moniquesntsilva@gmail.com>
2021-11-04 23:34:36 -0300

    feat: section destination initial structure

---
+++ travelnow/src/components/Destination/index.jsx
@@ -0,0 +1,19 @@
+import './destination.css';
+
+export default function Destination() {
+  return (
+    <section>
+      <picture>
+        <img src='./assets/figures/travel.svg' alt='Travel couple' />
+      </picture>
+      <div>
+        <h2>Travel to any corner of the World</h2>
+        <p>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type.
+        </p>
+        <button>Explore Destination</button>
+      </div>
+    </section>
+  );
+}
--- travelnow/src/pages/Home.jsx
+++ travelnow/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import '../styles/index.css';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Destination from '../components/Destination';
 
 export default function Home() {
   return (
     <div className='main-container'>
       <Header />
       <Hero />
+      <Destination />
     </div>
   );
 }
