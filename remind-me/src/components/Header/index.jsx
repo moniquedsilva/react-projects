28f3433b57d62da2bbcdee0f4f9fa0a1485411a6
Monique Silva <moniquesntsilva@gmail.com>
2021-11-10 23:02:27 -0300

    feat: desktop header

--- remind-me/src/components/Header/index.jsx
+++ remind-me/src/components/Header/index.jsx
@@ -1,12 +1,20 @@
 export default function index() {
   return (
-    <header className='w-content-scale 2xl:w-content-area mt-header-top flex justify-between items-center'>
+    <header className='w-content-scale 2xl:w-content-area mt-header-top text-primary flex justify-between items-center fixed'>
       <a href='/'>
         <img src='./assets/icons/logo.svg' alt='Remind Me Logo' />
       </a>
       <nav className='cursor-pointer md:hidden'>
         <img src='./assets/icons/nav-button.svg' alt='Open Menu' />
       </nav>
+      <nav className='hidden md:flex gap-x-9 xl:gap-x-header-nav lg:mr-56 xl:mr-96'>
+        <a href='#home'>Home</a>
+        <a href='#features'>Features</a>
+        <a href='#about-us'>About us</a>
+      </nav>
+      <button className='hidden md:block p-header-button-sm lg:p-header-button border-primary border-1.5 rounded-radius text-primary-2'>
+        About App
+      </button>
     </header>
   );
 }
--- remind-me/tailwind.config.js
+++ remind-me/tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      borderColor: {
+        primary: '#2D76F9',
+      },
+      borderWidth: {
+        1.5: '1.5px',
+      },
+      borderRadius: {
+        radius: '3.125rem',
+      },
+      textColor: {
+        primary: '#212121',
+        'primary-2': '#2D76F9',
+      },
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+      },
       width: {
         //Layout Dimensions
         container: '90rem',
@@ -12,6 +28,9 @@ module.exports = {
       spacing: {
         //Header
         'header-top': '25px',
+        'header-nav': '3.125rem',
+        'header-button': '11px 40px',
+        'header-button-sm': '8px 20px',
       },
     },
   },
