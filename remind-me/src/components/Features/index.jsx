18f1d40f418a33f247ac1bbc087e93502e580b0c
Monique Silva <moniquesntsilva@gmail.com>
2021-11-12 21:30:33 -0300

    fix: section display

---
+++ remind-me/src/components/Features/index.jsx
@@ -0,0 +1,11 @@
+export default function Features() {
+  return (
+    <section className='w-content-scale 2xl:w-content-area '>
+      <div>
+        <img src='./assets/icons/agenda.svg' alt='Agenda' />
+        <h2>Set a better deadline tasks</h2>
+        <p>You can make your duty deadlines better and more orderly</p>
+      </div>
+    </section>
+  );
+}
--- remind-me/src/pages/Home.jsx
+++ remind-me/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import Features from '../components/Features';
 
 export default function Home() {
   return (
-    <div className='w-screen 2xl:w-container flex justify-center'>
+    <div className='w-screen 2xl:w-container flex flex-col items-center'>
       <Header />
       <Hero />
+      <Features />
     </div>
   );
 }
