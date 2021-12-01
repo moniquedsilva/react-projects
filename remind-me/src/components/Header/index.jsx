a159c74a6db10e35a410882d9c804240c777b893
Monique Silva <moniquesntsilva@gmail.com>
2021-11-30 21:20:04 -0300

    feat: add background on header

--- remind-me/src/components/Header/index.jsx
+++ remind-me/src/components/Header/index.jsx
@@ -1,20 +1,22 @@
 export default function index() {
   return (
-    <header className='fixed flex items-center justify-between mt-header-top text-primary w-content-scale 2xl:w-content-area z-10'>
-      <a href='/'>
-        <img src='./assets/icons/logo.svg' alt='Remind Me Logo' />
-      </a>
-      <nav className='cursor-pointer md:hidden'>
-        <img src='./assets/icons/nav-button.svg' alt='Open Menu' />
-      </nav>
-      <nav className='hidden md:flex gap-x-9 xl:gap-x-header-nav lg:mr-56 xl:mr-96'>
-        <a href='#home'>Home</a>
-        <a href='#features'>Features</a>
-        <a href='#about-us'>About us</a>
-      </nav>
-      <button className='hidden md:block border-primary border-1.5 p-header-button-sm lg:p-header-button rounded-radius text-primary-2'>
-        About App
-      </button>
+    <header className='bg-white fixed flex justify-center py-2.5 w-full z-10'>
+      <div className='flex items-center justify-between text-primary w-content-scale 2xl:w-content-area'>
+        <a href='/'>
+          <img src='./assets/icons/logo.svg' alt='Remind Me Logo' />
+        </a>
+        <nav className='cursor-pointer md:hidden'>
+          <img src='./assets/icons/nav-button.svg' alt='Open Menu' />
+        </nav>
+        <nav className='hidden md:flex gap-x-9 xl:gap-x-header-nav lg:mr-56 xl:mr-96'>
+          <a href='#home'>Home</a>
+          <a href='#features'>Features</a>
+          <a href='#about-us'>About us</a>
+        </nav>
+        <button className='hidden md:block border-primary border-1.5 p-header-button-sm lg:p-header-button rounded-radius text-primary-2'>
+          About App
+        </button>
+      </div>
     </header>
   );
 }
